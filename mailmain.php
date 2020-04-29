2da5b35040faaee59ff08f281756eb1cdbcf0587
Ryan Sizemore <58410246+mastaryan@users.noreply.github.com>
2020-04-29 01:08:27 -0400

    cleaned up unused files. Updated index.html to link contact.html. Finished contact.html page

--- mailmain.php
+++ mailmain.php
@@ -2,12 +2,12 @@
 $name = $_POST['name'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
-$type = $_POST['type'];
+$priority = $_POST['priority'];
 $message = $_POST['message'];
-$formcontent=" From: $name \n Phone: $phone \n Type: $type \n Message: $message";
+$formcontent=" From: $name \n Phone: $phone \n Priority: $priority \n Message: $message";
 $recipient = "sizemore.ryan@gmail.com";
 $subject = "Sizemore & Associates Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You! We will be in touch with you soon." . " -" . "<a href='' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+echo "Thank You! We will be in touch with you soon." . " -" . "<a href='http://sizemoreprocessservices.com/' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 ?>
